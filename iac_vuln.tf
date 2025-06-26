3f1b2d8abbcc6d1a76dc0b370ea919f73a570ea3
OneMerguezMan <156898956+OneMerguezMan@users.noreply.github.com>
2025-06-26 12:06:46 +0200

    Delete iac_vuln.tf

--- iac_vuln.tf
+++
@@ -1,6 +0,0 @@
-# iac_vuln.tf
-# Vulnérabilité : S3 bucket public
-resource "aws_s3_bucket" "bad_bucket" {
-  bucket = "my-insecure-bucket"
-  acl    = "public-read" # Vulnérabilité : accès public
-} 
